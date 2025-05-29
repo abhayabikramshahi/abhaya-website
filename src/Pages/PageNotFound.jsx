7d96199a06fc357d13d2634fbd088aefec814183
abhayabikramshahi <abhayabikramshahiofficial@gmail.com>
2025-05-29 18:39:34 +0545

    add page not found

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+// src/pages/NotFound.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function PageNotFound() {
+const NotFound = () => {
   return (
-    <div>404</div>
-  )
-}
+    <div className="min-h-screen flex flex-col items-center justify-center text-center p-4">
+      <h1 className="text-6xl font-bold mb-4">404</h1>
+      <p className="text-xl mb-6">Oops! Page not found 😓</p>
+      <Link to="/" className="text-blue-500 underline">
+        Go back home
+      </Link>
+    </div>
+  );
+};
 
-export default PageNotFound;
+export default NotFound;
