d5042a6d1f122f5c8d664042506d064b572dd3e8
abhayabikramshahi <abhayabikramshahiofficial@gmail.com>
2025-05-29 18:59:56 +0545

    add professional

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'; // Import the tailwindcss plugin
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    tailwindcss(), // Add the tailwindcss plugin here
   ],
-})
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+});
