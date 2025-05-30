d19574d34cd1a00eb7938fc8bafee8f6a5be9bf0
abhayabikramshahi <abhayabikramshahiofficial@gmail.com>
2025-05-30 15:10:35 +0545

    changes fro netlify

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,15 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: undefined,
       },
     },
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
   },
 });
